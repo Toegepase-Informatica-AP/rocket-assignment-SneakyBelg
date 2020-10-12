e51c74d74c254d2de6d06dddcb25ab3277b60344
SneakyBelg <43534278+SneakyBelg@users.noreply.github.com>
2020-10-12 11:29:44 +0200

    Added rocket ship project

---
+++ RocketShipGame Matthias Verschorren/Assets/Scripts/ButtonLevelLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLevelLoad : MonoBehaviour
+{
+    public int mLevelToLoad;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(mLevelToLoad);
+    }
+}
---
+++ RocketShipGame Matthias Verschorren/Assets/Scripts/RocketController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketController : MonoBehaviour
+{
+    [SerializeField] float thrusterForce = 1000f;
+    [SerializeField] float tiltingForce = 100f;
+
+    bool thrust = false;
+    public Text coinText;
+    public int coinCounter = 0;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float tilt = Input.GetAxis("Horizontal");
+
+        thrust = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Space);
+
+        if (!Mathf.Approximately(tilt, 0f))
+        {
+            rb.freezeRotation = true;
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + (new Vector3(0f, 0f, tilt * tiltingForce * Time.deltaTime)));
+        }
+        rb.freezeRotation = false;
+        coinText.text = "Coins: " + coinCounter;
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust)
+        {
+            rb.AddRelativeForce(Vector3.up * thrusterForce * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Coin>())
+        {
+            coinCounter++;
+            Destroy(other.gameObject);
+        }
+    }
+
+}
